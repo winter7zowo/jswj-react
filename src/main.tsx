96346dd09313d6546ac6c467a6b4fb7723110642
fuelatank <fuelatank@gmail.com>
2025-03-20 16:36:44 +0800

    Add react router

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Route, Routes } from "react-router"
 import '@ant-design/v5-patch-for-react-19'
 import './index.css'
 import App from './App.tsx'
+import ContentViewPage from './ContentViewPage.tsx'
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
-        <App />
+        <BrowserRouter>
+            <Routes>
+                <Route index element={<App />} />
+                <Route path="content" element={<ContentViewPage />} />
+            </Routes>
+        </BrowserRouter>
     </StrictMode>,
 )
