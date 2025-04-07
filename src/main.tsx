8a73bf00a154db1d0b853bf0f18c62fd9447cbb6
winter7zowo <winter7z@163.com>
2025-04-07 14:04:00 +0800

    update main page

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,21 @@
-import { createRoot } from 'react-dom/client'
+import '@ant-design/v5-patch-for-react-19'
 import './index.css'
-import App from './App'
+import App from './App.tsx'
+import ContentViewPage from './pages/ContentViewPage.tsx'
+import ManageTagPage from './pages/ManageTagPage.tsx'
+import { createRoot } from 'react-dom/client'
+import { StrictMode } from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router'
 
 createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<App />} />
+        <Route path="content" element={<ContentViewPage />} />
+        <Route path="tags" element={<ManageTagPage />} />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>,
   <App />
 )
